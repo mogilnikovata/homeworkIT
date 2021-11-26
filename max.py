971449bb3d48f370f07ae152932533596ea90f22
mogilnikovata <tanya.mogilnikova@mail.ru>
2021-11-26 21:11:38 +0300

    max.py added

---
+++ max.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+values=[10,15,87,1,16,118,64,89]
+
+
+# In[2]:
+
+
+max_value=max(values)
+
+
+# In[3]:
+
+
+print("Наибольшее значение:", max_value)
+
